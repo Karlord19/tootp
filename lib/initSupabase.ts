327e07a528fcbbfadab47ad45e2d6af6ba023638
Klára Pernicová <klapernicova@gmail.com>
2024-11-27 10:20:18 +0100

    connect to supabase, table not found :(

---
+++ lib/initSupabase.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+      detectSessionInUrl: false,
+    },
+  });
+
+  // alter role "role_name" with bypassrls;
